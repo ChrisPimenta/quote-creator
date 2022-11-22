abb12c060d271775c88658a0529798bfd34ec4d0
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-22 16:54:29 +0100

    Add logo to header

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,6 +4,7 @@ import classes from './MainNavigation.module.css'
 const MainNavigation = () => {
     return (
         <header className={classes.header}>
+            <div className={classes.logo}>Chrispy Quotes</div>
             <nav className={classes.nav}>
                 <ul>
                     <li><NavLink to='/quotes' activeClassName={classes.active}> All Quotes</NavLink></li>
