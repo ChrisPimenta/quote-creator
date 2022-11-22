4336d8d8e77bcd1c90ac4f6ca3e041945c2c8883
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-22 16:53:37 +0100

    Add dummy data to quote detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,17 +1,36 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Route } from 'react-router-dom';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
     const params = useParams();
 
-    // Get Highlighted quote from list using params
-    console.log(params);
+    // TODO: Remove after implementing db and fetch
+    const dummyQuotes = [
+        {
+            id: 1,
+            author: 'James Franco',
+            text: 'Some cool quote.'
+        },
+        {
+            id: 2,
+            author: 'Chris Pimenta',
+            text: 'Winners never quit and quitters never win.'
+        },
+    ]
+
+    const quote = dummyQuotes.find(q => q.id === Number(params.quoteId));
+
+    if (!quote) {
+        return <p>No quote found.</p>
+    }
 
     return (
         <>
-            <HighlightedQuote />
-            <Comments />
+            <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path='/quotes/:quoteId/comments'>
+                <Comments />
+            </Route>
         </>
     )
 }
