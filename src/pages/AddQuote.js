26bb0e961716b80474940b519ff13a882eb6a174
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-22 17:29:51 +0100

    Implement sorting of quotes through query params

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,8 +1,12 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from 'react-router-dom';
 
 const AddQuotes = () => {
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+        history.push('/quotes');
     }
 
     return (
